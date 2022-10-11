e204956595d27e29daf084e048868d6f7d781b90
Ruslan Abbasov <abbasov.ruslan1999@yndex.ru>
2022-10-11 12:53:56 +0400

    Initial Commit

---
+++ Code-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Code-example
+//
+//  Created by Ruslan Abbasov on 11.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
